3a079411610350eed677034f9564d55cab63bb09
Paul <baierpa@g.cofc.edu>
2017-11-09 20:55:20 +0000

    working on groups_users table cucumber

---
+++ features/step_definitions/filter_by_group_steps.rb
@@ -0,0 +1,12 @@
+When /^(?:|I )check the following groups: "([^"]*)"$/ do |group_list|
+    
+end
+
+# Given /the following groups_users exist/ do |groups_users_table|
+#   groups_users_table.hashes.each do |gu|
+#         groups_users.creat
+# # 	  groups.users << groups_users
+#   	 # Group.create!(group)
+
+#   end
+# end
