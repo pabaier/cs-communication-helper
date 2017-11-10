b4c12e3512b5eb15ca0631fd673eac2a234d93df
Paul <baierpa@g.cofc.edu>
2017-11-10 21:09:15 +0000

    gu works

--- features/step_definitions/filter_by_group_steps.rb
+++ features/step_definitions/filter_by_group_steps.rb
@@ -2,11 +2,9 @@ When /^(?:|I )check the following groups: "([^"]*)"$/ do |group_list|
     
 end
 
-# Given /the following groups_users exist/ do |groups_users_table|
-#   groups_users_table.hashes.each do |gu|
-#         groups_users.creat
-# # 	  groups.users << groups_users
-#   	 # Group.create!(group)
+Given /the following groups_users exist/ do |groups_users_table|
+  groups_users_table.hashes.each do |gu|
+    Group#users << gu
 
-#   end
-# end
+  end
+end
