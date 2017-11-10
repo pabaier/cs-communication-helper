ecbb0e098c44d666af1715bd2c4f7dc2adc71aa8
Paul <baierpa@g.cofc.edu>
2017-11-10 22:38:54 +0000

    checkboxes not recognized by capybara

--- features/step_definitions/filter_by_group_steps.rb
+++ features/step_definitions/filter_by_group_steps.rb
@@ -1,9 +1,17 @@
-When /^(?:|I )check the following groups: "([^"]*)"$/ do |group_list|
-    
+When /^(?:|I )check the following groups: ([^"]*)$/ do |group_list|
+  group_list.split.each do |group|
+    group_id = Group.find_by_title(group).id
+    check("groups[#{group}]")
+    # find("groups[#{group}]").click
+    # find("groups_#{group_id}").click
+
+  end
 end
+
 
 Given /the following groups_users exist/ do |groups_users_table|
   groups_users_table.hashes.each do |gu|
+    # https://apidock.com/rails/ActiveRecord/Associations/ClassMethods/has_and_belongs_to_many
     Group#users << gu
 
   end
