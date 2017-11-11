bd1aeae2e58f8645d6d4adf21fc64842193781c8
Paul <baierpa@g.cofc.edu>
2017-11-11 17:43:43 +0000

    updated seeds

--- features/step_definitions/filter_by_group_steps.rb
+++ features/step_definitions/filter_by_group_steps.rb
@@ -8,11 +8,9 @@ When /^(?:|I )check the following groups: ([^"]*)$/ do |group_list|
   end
 end
 
-
 Given /the following groups_users exist/ do |groups_users_table|
   groups_users_table.hashes.each do |gu|
     # https://apidock.com/rails/ActiveRecord/Associations/ClassMethods/has_and_belongs_to_many
     Group#users << gu
-
   end
 end
