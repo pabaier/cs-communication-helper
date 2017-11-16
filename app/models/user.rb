b78a30198b3ccdf60d9aaf48ec4566a0059f3d4e
Paul <baierpa@g.cofc.edu>
2017-11-16 18:51:42 +0000

    cleaned up a little

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ActiveRecord::Base
 
   # a class method import, with file passed through as an argument
   def self.import(file)
-    fields = User.column_names
-    fields.delete("id")
+    fields = User.column_names - ["id"]
+    # fields.delete("id")
     # a block that runs through a loop in our CSV data
     CSV.foreach(file.path, headers: false) do |row|
       user_hash = Hash[fields.zip(row)] 
