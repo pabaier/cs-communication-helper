1b138acddb8f43336445d2ae38334e70090cb976
Paul <baierpa@g.cofc.edu>
2017-11-16 18:07:01 +0000

    csv upload without headers works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ActiveRecord::Base
-    require 'csv'
-    has_and_belongs_to_many :groups
-    self.primary_key = "id"
-    
-    def self.all_students
-        User.where(:status => ["Student", "Alumni"])
-    end
+  require 'csv'
+  has_and_belongs_to_many :groups
+  self.primary_key = "id"
+  
+  def self.all_students
+      User.where(:status => ["Student", "Alumni"])
+  end
 
   # a class method import, with file passed through as an argument
   def self.import(file)
+    fields = ["first_name", "last_name", "email", "password", "status", "graduation_date"]
     # a block that runs through a loop in our CSV data
-    CSV.foreach(file.path, headers: true) do |row| 
+    CSV.foreach(file.path, headers: false) do |row|
+      x = Hash[fields.zip(row)] 
       # creates a user for each row in the CSV file
-      User.create! row.to_hash 
+      User.create! x#row.to_hash 
     end 
   end 
     
