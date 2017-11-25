aee4f6f41bbb58b52196fe22caa5e06b17bb543b
Ahana <roya@g.cofc.edu>
2017-11-25 15:32:18 +0000

    cucumber tests scenarios

--- features/bulk_import.feature
+++ features/bulk_import.feature
@@ -6,11 +6,11 @@ Feature: CSV file upload
 Scenario: Uploading a valid file with data for 3 new users
   Given I am on the users page
   When I upload a file with valid data for 3 new users
-  Then the number of products should change to 3
-  And I should be sent to the products index page
+  Then the number of users should increase by 3
+  And I should be on the users page
 
 Scenario: Uploading a bad file
   Given I am on the users page
   When I upload a bad file
-  Then the number of products should not change
-  And I should see the file upload page again
+  Then the number of users should not change
+  And I should be on the users page
--- features/step_definitions/bulk_import_steps.rb
+++ features/step_definitions/bulk_import_steps.rb
@@ -3,9 +3,17 @@ When /^(?:|I )upload a file with valid data for 3 new users$/ do
 #   attach_file(:csv_file, File.join(RAILS_ROOT, 'features', 'upload_files', 'good_csv.csv'))
   click_button "Import CSV"
 end
+
+Then("the number of users should increase by {int}") do |int|
+  # Write code here that turns the phrase above into concrete actions
+end
  
 When /^(?:|I )upload a bad file$/ do
   attach_file(:file, File.join('features', 'upload_files', 'bad_csv.csv'))
 #   attach_file(:csv_file, File.join(RAILS_ROOT, 'features', 'upload_files', 'bad_csv.csv'))
   click_button "Import CSV"
+end
+
+Then("the number of users should not change") do
+  # Write code here that turns the phrase above into concrete actions
 end
